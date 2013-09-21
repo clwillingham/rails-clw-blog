bb7f0a0920c8719545dc64ef4ba5d495fe10cd2e
Chris Willingham <clwillingham@gmail.com>
2013-09-21 15:56:44 -0400

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+if Page.where(path: '') == 0
+  Page.create(
+      title: 'Home',
+      path: ''
+  )
+end
