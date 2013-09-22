fe34b29fcd12257a7f9ec0d61e9d70d1884327d3
Chris Willingham <clwillingham@gmail.com>
2013-09-22 16:31:34 -0400

    oops

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  if Rails.env.development
+  if Rails.env.development?
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   else
     devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
